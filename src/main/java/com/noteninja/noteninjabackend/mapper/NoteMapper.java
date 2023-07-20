e57d8623163aeedc9830d9df5e08bceccb828d92
ilija-malkier <ilija.radojkovic@malkier.rs>
2023-07-20 11:57:06 +0200

    Added  NoteMapper.java

---
+++ src/main/java/com/noteninja/noteninjabackend/mapper/NoteMapper.java
@@ -0,0 +1,25 @@
+package com.noteninja.noteninjabackend.mapper;
+
+import com.noteninja.noteninjabackend.model.entity.Note;
+import com.noteninja.noteninjabackend.model.request.SaveNoteRequest;
+import com.noteninja.noteninjabackend.model.response.SavedNoteResponse;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import java.util.UUID;
+
+@Mapper(
+        imports = {UUID.class},
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
+public interface NoteMapper {
+
+    @Mapping(target = "id",expression = "java(UUID.randomUUID())")
+    @Mapping(target = "createdAt",expression = "java(LocalDateTime.now())")
+    Note fromSaveNoteRequestToNote(SaveNoteRequest saveNoteRequest);
+
+    SavedNoteResponse fromNoteToSavedNoteResponse(Note note);
+}
