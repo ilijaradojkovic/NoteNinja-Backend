4c53ebda6f7a608df40073de076cca432b65408b
ilija-malkier <ilija.radojkovic@malkier.rs>
2023-07-20 12:19:58 +0200

    Changed UUID type to be stored like varchar

--- src/main/java/com/noteninja/noteninjabackend/model/entity/Note.java
+++ src/main/java/com/noteninja/noteninjabackend/model/entity/Note.java
@@ -1,12 +1,16 @@
 package com.noteninja.noteninjabackend.model.entity;
 
 import com.noteninja.noteninjabackend.model.NoteType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -19,6 +23,8 @@ public class Note {
 
     @Id
     @GeneratedValue
+    @Column(name = "id", columnDefinition = "varchar(36)")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     private UUID id;
 
     private String title;
