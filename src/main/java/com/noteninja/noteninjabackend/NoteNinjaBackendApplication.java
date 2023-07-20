e8cf67c68edeb3e347ded96cc659e4970a3b7157
ilija-malkier <ilija.radojkovic@malkier.rs>
2023-07-20 11:18:07 +0200

    init

---
+++ src/main/java/com/noteninja/noteninjabackend/NoteNinjaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.noteninja.noteninjabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteNinjaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteNinjaBackendApplication.class, args);
+    }
+
+}
